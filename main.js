16510ee7953c084a09a0aa1616a42c1e7f238a6c
Ivan Kantur <ivankantur87@gmail.com>
2022-10-23 14:55:53 +0300

    main: add 8 task

--- main.js
+++ main.js
@@ -76,3 +76,15 @@ arr.forEach(function (value, index, array){
 })
 console.log(`Нечётных чисел: ${countOdd};` +' '+ `Чётных чисел: ${countHonest}` +' '+ `Нулей: ${countZero}`)
 console.log(`Элементов не являющихся числами: ${countElse} штук.`)
+
+
+// Задание №8 Создайте произвольный массив Map. Получите его ключи и выведите в консоль все значения в виде «Ключ — Х, значение — Y».
+let myMap = new Map();
+myMap.set(1, "Monday");
+myMap.set(true, "true");
+myMap.set("one", 1);
+
+for (let name of myMap.keys()){
+    let value = myMap.get(name);
+    console.log(`Ключ - ${name}, зничение - ${value}`);
+}
